f366bacc48482bb233ba94cbe3b1c0abbb5b5d8b
Rahil <mohamedrshaikh@gmail.com>
2020-04-09 19:59:11 +0400

    Daily problems
    Added few daily problems

--- LeetCode/src/com/rs4u/algos/recursions/Fib.java
+++ LeetCode/src/com/rs4u/algos/recursions/Fib.java
@@ -2,17 +2,20 @@ package com.rs4u.algos.recursions;
 
 public class Fib {
 
-	private static int fib(int n, int prev, int curr) {
-		if (n <= 0)
-			return prev + curr;
-		return fib(--n, curr, prev + curr);
-
+	static int fib(int i, int[] mem) {
+		if (i == 0)
+			return 0;
+		if (i == 1)
+			return 1;
+		if (mem[i] == 0) {
+			mem[i] = fib(i - 1, mem) + fib(i - 2, mem);
+		}
+		return mem[i];
 	}
 
 	public static void main(String[] args) {
-		int prev = 0;
-		int current = 1;
-		int n = 1;
-		System.out.println(fib(n - 2, prev, current));
+		int i = 6;
+		int[] mem = new int[i + 1];
+		System.out.println(fib(i, mem));
 	}
 }
