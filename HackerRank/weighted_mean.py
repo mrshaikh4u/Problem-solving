5afb504dd2a46727253213f6d2b7186a73d35504
Rahil <mohamedrshaikh@gmail.com>
2020-06-01 06:22:41 +0400

    Added standard deviation python code

--- HackerRank/weighted_mean.py
+++ HackerRank/weighted_mean.py
@@ -1,12 +1,25 @@
+import math
 def weighted_mean(numbers:list,weights:list):
-    sum = 0.0
-    weight_sum = 0.0
+    summation = 0.0
+    weight_summation = 0.0
     for i in range(len(numbers)):
-        sum+=(numbers[i]*weights[i])
-        weight_sum+=weights[i]
-    print('{:.1f}'.format(sum/weight_sum))
+        summation+=(numbers[i]*weights[i])
+        weight_summation+=weights[i]
+    print('{:.1f}'.format(summation/weight_summation))
+
+def standard_deviation(numbers:list):
+    summation = sum(numbers)
+    mean = summation/len(numbers)
+    summation=0.0
+    for i in numbers:
+        summation+=((i-mean)**2)
+    summation/=len(numbers)
+    print('{:.1f}'.format(math.sqrt(summation)))
+
+
 
 n = int(input())
 numbers = list(map(int,input().split()))
-weights = list(map(int,input().split()))
-weighted_mean(numbers,weights)
+standard_deviation(numbers)
+# weights = list(map(int,input().split()))
+# weighted_mean(numbers,weights)
